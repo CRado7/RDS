b535ee9ad6634dbb4a6d34bfd8e8d8a4d14a5698
Christopher <christopher.ferraro34@gmail.com>
2025-10-01 19:01:49 -0400

    webp images

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
   plugins: [
-    react(),
+    react(), 
+    viteImagemin({
+      webp: {
+        quality: 80, // adjust quality (0-100)
+      },
+      gifsicle: false,
+      optipng: false,
+      mozjpeg: false,
+      svgo: false,
+    }),
     viteStaticCopy({
       targets: [
         {
