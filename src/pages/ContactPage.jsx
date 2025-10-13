9a10555a03688b23e213b4628bb62ef6014855ee
Christopher <christopher.ferraro34@gmail.com>
2025-10-13 13:25:37 -0400

    Update ContactPage.jsx
    fixed phone number

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
       <section className="container contact-container page-content">
         <p className="contact-tagline">If it ain't Rado, it ain't rad.</p>
         <a href="mailto:hello@radodesignstudio.com">email</a>
-        <a href="tel:+7743923956">phone</a>
+        <a href="tel:+17743923956">phone</a>
       </section>
     </div>
   );
